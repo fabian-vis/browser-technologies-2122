20e3b9e6f32d8c474e06c0c2ff5dd9e1a4a7200b
Fabian Vis <70572501+fabian-vis@users.noreply.github.com>
2022-04-06 12:45:24 +0200

    Works on Old IOS devices

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+var form = document.getElementById('formID'); // form has to have ID: <form id="formID">
+form.noValidate = true;
+form.addEventListener('submit', function (event) { // listen for form submitting
+    if (!event.target.checkValidity()) {
+        event.preventDefault(); // dismiss the default functionality
+        document.getElementById('errorMessageDiv').classList.remove("hidden");
+    }
+}, false);
