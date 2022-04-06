c3299fee7f49496a21dc3c05f21d6b1ce2011fee
Fabian Vis <70572501+fabian-vis@users.noreply.github.com>
2022-04-06 13:52:24 +0200

    Save on reload

--- public/js/script.js
+++ public/js/script.js
@@ -6,3 +6,18 @@ form.addEventListener('submit', function (event) { // listen for form submitting
         document.getElementById('message').classList.remove("hidden");
     }
 }, false);
+
+// input to track
+var field = document.getElementById("field");
+if (sessionStorage.getItem("autosave")) {
+    // Restore a content of the input
+    field.value = sessionStorage.getItem("autosave");
+}
+
+// Listen for changes in the input field
+field.addEventListener("change", function () {
+    // save value into sessionStorage object 
+    sessionStorage.setItem("autosave", field.value);
+});
+
+console.log(sessionStorage.getItem("autosave"))
