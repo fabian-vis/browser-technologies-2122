b72a479ac10003d1ff25ec6a1370cece8b1339c8
Fabian Vis <70572501+fabian-vis@users.noreply.github.com>
2022-04-06 14:31:05 +0200

    Works on most devices

--- public/js/script.js
+++ public/js/script.js
@@ -8,16 +8,41 @@ form.addEventListener('submit', function (event) { // listen for form submitting
 }, false);
 
 // input to track
-var field = document.getElementById("field");
+var naam = document.getElementById("naam");
+var nummertje = document.getElementById("nummertje");
+var selection = document.getElementById("selection")
 if (sessionStorage.getItem("autosave")) {
     // Restore a content of the input
-    field.value = sessionStorage.getItem("autosave");
+    naam.value = sessionStorage.getItem("autosave");
 }
 
+if (sessionStorage.getItem("autosave2")) {
+    // Restore a content of the input
+    nummertje.value = sessionStorage.getItem("autosave2");
+}
+
+if (sessionStorage.getItem("autosavee")) {
+    // Restore a content of the input
+    selection.value = sessionStorage.getItem("autosave3");
+}
+
+
 // Listen for changes in the input field
-field.addEventListener("change", function () {
+naam.addEventListener("change", function () {
     // save value into sessionStorage object 
-    sessionStorage.setItem("autosave", field.value);
+    sessionStorage.setItem("autosave", naam.value);
+});
+
+nummertje.addEventListener("change", function () {
+    // save value into sessionStorage object 
+    sessionStorage.setItem("autosave2", nummertje.value);
+});
+
+selection.addEventListener("change", function () {
+    // save value into sessionStorage object 
+    sessionStorage.setItem("autosave3", selection.value);
 });
 
 console.log(sessionStorage.getItem("autosave"))
+console.log(sessionStorage.getItem("autosave2"))
+console.log(sessionStorage.getItem("autosave3"))
